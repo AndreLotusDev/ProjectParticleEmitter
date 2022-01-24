4353c06395fd9457f1a3493b81c7a0996156d00d
Andre Soares Gomes <andrsoares953@yahoo.com>
2022-01-24 01:28:34 -0300

    add notes.ts

---
+++ src/notes.ts
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+$('a#documentation-button').attr({target: '_blank', 
+                    href  : '/Documentation.docx'});
+
+$('a#json-download-button').attr({target: '_blank', 
+href  : '/fireworks_example.json'});
+
+$('a#talk-with-me-button').attr({target: '_blank', 
+                href  : 'https://wa.me/5541988664960?text=Hello'});
+
+export {}
